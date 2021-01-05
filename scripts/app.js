627f4467d29a01bdfd24a1e2d034ff5acfd188dd
Ahmed Mikkawe <dev.ahmedmikkawe@gmail.com>
2021-01-05 12:43:45 +0200

    adds operate functionality that takes operator and two numbers and calls the proper function

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,19 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+// operation function decides the operation based on the operator
+const operate = (operator, num1, num2) =>{
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+};
